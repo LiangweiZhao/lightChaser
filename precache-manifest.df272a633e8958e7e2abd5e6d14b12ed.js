966eab78d45d207aa7aa624d5757931deed32508
liangwei zhao <liangweizhao@liangweideMacBook-Pro.local>
2019-11-30 02:06:28 -0800

    Updates

---
+++ precache-manifest.df272a633e8958e7e2abd5e6d14b12ed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2d0c4c2130d1f5676e0835a6220231",
+    "url": "/light_chaser/index.html"
+  },
+  {
+    "revision": "823bc6eca9b28b44b0d7",
+    "url": "/light_chaser/static/css/2.2f67b891.chunk.css"
+  },
+  {
+    "revision": "06b8ad78a09b8d01c681",
+    "url": "/light_chaser/static/css/main.622061d4.chunk.css"
+  },
+  {
+    "revision": "823bc6eca9b28b44b0d7",
+    "url": "/light_chaser/static/js/2.88c8e90f.chunk.js"
+  },
+  {
+    "revision": "06b8ad78a09b8d01c681",
+    "url": "/light_chaser/static/js/main.043e4b1d.chunk.js"
+  },
+  {
+    "revision": "5a806a5dc0e5443a5c2b",
+    "url": "/light_chaser/static/js/runtime-main.e5c0582b.js"
+  },
+  {
+    "revision": "a1daf3ac5f0dbf902c8673e8f135079f",
+    "url": "/light_chaser/static/media/USC-Games_CardOnWhite_Horiz.a1daf3ac.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/light_chaser/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/light_chaser/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "47bf98a6a4409a4b9c5fcc2c56be913e",
+    "url": "/light_chaser/static/media/scene_bg.47bf98a6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/light_chaser/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/light_chaser/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/light_chaser/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/light_chaser/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0d811f32787dfa0c1d2f16a281832b54",
+    "url": "/light_chaser/static/media/start_bg.0d811f32.jpeg"
+  },
+  {
+    "revision": "94a54aeff52e774923e742330b211c32",
+    "url": "/light_chaser/static/media/teamer_bg.94a54aef.jpg"
+  }
+]);
